b5ed9088e30c5878d937cb3b151f61d027906ecd
hyominchoi <hyomin.choi@gmail.com>
2015-11-23 21:14:46 -0500

    MKDIRs  1. Udacity Lessons 2. Starting Code

--- Lessons_and_Starting_Code/Lesson_2/00_Conference_Central/conference.py
+++ Lessons_and_Starting_Code/Lesson_2/00_Conference_Central/conference.py
@@ -14,12 +14,16 @@ __author__ = 'wesc+api@google.com (Wesley Chun)'
 
 
 from datetime import datetime
+import json
+import os
+import time
 
 import endpoints
 from protorpc import messages
 from protorpc import message_types
 from protorpc import remote
 
+from google.appengine.api import urlfetch
 from google.appengine.ext import ndb
 
 from models import Profile
@@ -27,15 +31,12 @@ from models import ProfileMiniForm
 from models import ProfileForm
 from models import TeeShirtSize
 
-from utils import getUserId
-
 from settings import WEB_CLIENT_ID
 
 EMAIL_SCOPE = endpoints.EMAIL_SCOPE
 API_EXPLORER_CLIENT_ID = endpoints.API_EXPLORER_CLIENT_ID
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 
 @endpoints.api( name='conference',
                 version='v1',
@@ -63,23 +64,24 @@ class ConferenceApi(remote.Service):
 
     def _getProfileFromUser(self):
         """Return user Profile from datastore, creating new one if non-existent."""
-        user = endpoints.get_current_user()
-        if not user:
-            raise endpoints.UnauthorizedException('Authorization required')
-
-        # get Profile from datastore
-        user_id = getUserId(user)
-        p_key = ndb.Key(Profile, user_id)
-        profile = p_key.get()
-        # create new Profile if not there
+        ## TODO 2
+        ## step 1: make sure user is authed
+        ## uncomment the following lines:
+        # user = endpoints.get_current_user()
+        # if not user:
+        #     raise endpoints.UnauthorizedException('Authorization required')
+        profile = None
+        ## step 2: create a new Profile from logged in user data
+        ## you can use user.nickname() to get displayName
+        ## and user.email() to get mainEmail
         if not profile:
             profile = Profile(
-                key = p_key,
-                displayName = user.nickname(),
-                mainEmail= user.email(),
+                userId = None,
+                key = None,
+                displayName = "Test", 
+                mainEmail= None,
                 teeShirtSize = str(TeeShirtSize.NOT_SPECIFIED),
             )
-            profile.put()
 
         return profile      # return Profile
 
@@ -96,7 +98,6 @@ class ConferenceApi(remote.Service):
                     val = getattr(save_request, field)
                     if val:
                         setattr(prof, field, str(val))
-            prof.put()
 
         # return ProfileForm
         return self._copyProfileToForm(prof)
@@ -108,17 +109,15 @@ class ConferenceApi(remote.Service):
         """Return user profile."""
         return self._doProfile()
 
-
-    @endpoints.method(ProfileMiniForm, ProfileForm,
+    # TODO 1
+    # 1. change request class
+    # 2. pass request to _doProfile function
+    @endpoints.method(message_types.VoidMessage, ProfileForm,
             path='profile', http_method='POST', name='saveProfile')
     def saveProfile(self, request):
         """Update & return user profile."""
-        return self._doProfile(request)
+        return self._doProfile()
 
-
-# - - - Conference objects - - - - - - - - - - - - - - - - - - -
-
-# TODO
 
 # registers API
 api = endpoints.api_server([ConferenceApi]) 
